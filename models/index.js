7de6128f86994280d4a1fe2e33c9c777d70f1558
rhw <ruohanw89@gmail.com>
2020-08-31 06:34:36 -0600

    define association between post table and user table

--- models/index.js
+++ models/index.js
@@ -2,5 +2,14 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// Post belongsTo User
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+// User have many Post
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+});
+
 // deconstruct and export all models
 module.exports = { User, Post };
