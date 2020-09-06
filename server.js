c4ecfe1644c45095d5600665bf962e06b72a538a
rhw <ruohanw89@gmail.com>
2020-09-06 00:58:00 -0600

    set engine to be handlebars

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
 const sequelize = require('./config/connection');
+// import from express-handlebars
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// set handlebars as teh template engine of choice
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(routes);
