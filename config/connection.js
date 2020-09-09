b9b82bd77798206402ba6c01a924a758bc824021
rhw <ruohanw89@gmail.com>
2020-09-08 23:49:22 -0600

    change connection port for heroku

--- config/connection.js
+++ config/connection.js
@@ -15,9 +15,7 @@ if (process.env.JAWSDB_URL) {
     {
       host: 'localhost',
       dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
+      port: 3306,
     },
   );
 }
